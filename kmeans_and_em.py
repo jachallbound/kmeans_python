4901a91a12e306365a444c6f595a1f509e214a1b
Jacob Hall <jachallbound@gmail.com>
2021-10-16 14:54:40 -0400

    Working driver for KMeans class work on 3d plot

--- kmeans_and_em.py
+++ kmeans_and_em.py
@@ -3,3 +3,18 @@ Documentation, License etc.
 
 @package kmeans_and_em
 '''
+import numpy as np
+import plotly.express as px
+
+from src.KMeans import KMeans
+
+K = 4
+data = np.random.rand(100,100,100)*100
+k = KMeans(K, data)
+
+K_mat = k.get_means(True)
+
+print(K_mat)
+
+#fig = px.scatter_3d(x=data[0], y=data[1], z=data[2])
+#fig.show()
