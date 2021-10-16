70688e8d005a8088a5d037e83d30852f42b958f4
Jacob Hall <jachallbound@gmail.com>
2021-10-16 15:01:35 -0400

    KMeans random working for arbitrary K and dimensions added readme ignoring kdev project files empty file for EM classification

---
+++ README.md
@@ -0,0 +1,2 @@
+# kmeans_and_em
+Attempt at writing K-Means and EM in Python
---
+++ src/KMeans.py
@@ -0,0 +1,40 @@
+import numpy as np
+
+class KMeans:
+    """Class for performing KMeans classification.
+    Find K integer means in a cluster of data that optimizes the decision
+    boundaries.
+    Choose any integer of K means to find.
+    Choose any dimension of cluster data to operate on.
+    """
+    def __init__(self, K, data):
+        """Initialize Class
+
+        Parameters:
+        ---
+        K : *int*
+            Amount of means you wish to find in the data.
+        data : *array_like*
+            Cluster of data. Any amount of dimensions.
+            Do not have a singleton dimension.
+        """
+        self.K = K
+        self.data = data
+        self.ndim = data.ndim
+
+    def get_means(self, random=False):
+        """Method to calculate and return new K means
+
+        """
+        if random:
+            # Get random means
+            K_mat = np.zeros([self.K, self.ndim])
+            for k in range(self.K):
+                for d in range(self.ndim):
+                    K_mat[k,d] = np.random.uniform(
+                        np.min(self.data[d]),np.max(self.data[d])
+                        )
+        else:
+            # Get mean from data
+            K_mat = 1
+        return K_mat
