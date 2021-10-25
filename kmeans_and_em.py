ee259770891480ddabdfa5f3fd4f521d69169b8a
Jacob Hall <jachallbound@gmail.com>
2021-10-25 03:23:27 -0400

    fully working driver script plot 2,3D data, hists, and print classified means

--- kmeans_and_em.py
+++ kmeans_and_em.py
@@ -3,6 +3,7 @@ Documentation, License etc.
 
 @package kmeans_and_em
 '''
+import copy
 import ipdb
 import matplotlib.pyplot as plt
 import numpy as np
@@ -15,7 +16,7 @@ from src.GaussianDistributionGenerator import generate_gaussian_data
 from src.PlotData import SubPlotData
 
 "Data Parameters"
-K = 5
+K = 3 # Number of distributions to create and means to classify
 dims = 3 # Dimensions of Data
 samples = 100000 # Number of samples in each distribution
 
@@ -32,18 +33,37 @@ covs =   gmm_dict['covs']
 "Create KMeans object with our observed data"
 km = KMeans(K, data, labels)
 
-for i in range(1):
+labels_mapped_prev = np.zeros(labels.shape)
+labels_changed_percent = 1.0
+i = 0
+while labels_changed_percent > 0.01:
+#for i in range(4):
+    "If first loop, choose random starting means"
     random_means = False
     if i == 0:
         random_means = True
+
+    "Generate matrix of means and map points to them"
     K_mat = km.generate_means(random_means)
-    print(K_mat)
-
+    print(f"K Means {K_mat}")
     labels_mapped = km.map_points(K_mat)
-    print(labels_mapped)
 
-"Plot 2-D data"
-if data.shape[0] == 2|3:
+    "Check how many points have changed labels since last loop"
+    labels_changed_percent = sum(labels_mapped != labels_mapped_prev)/samples
+    labels_mapped_prev = copy.deepcopy(labels_mapped)
+    i += 1
+    print(f"Iteration: {i}, Labels changed: {labels_changed_percent*100:2.2f}%")
+
+"Print detected means and true means"
+"Sort means matrices to check mean error"
+"TODO: Need to relabel in order to check classification error"
+print(f"\n\n")
+print(f"K Means\n{np.sort(K_mat, axis=0)}")
+print(f"True Means\n{np.sort(means, axis=0)}")
+print(f"Mean error {np.mean(km.euclidean_distance(K_mat, means, axis=1)):.2f} (not a percentage)")
+
+"Plot 2-D or 3-D data"
+if data.shape[0] == 2 or data.shape[0] == 3:
     labels_both = np.stack((labels, labels_mapped))
     means_both = np.stack((means, K_mat))
     SubPlotData(K, data, labels_both, means_both)
